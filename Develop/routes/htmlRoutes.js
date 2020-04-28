8e0cf25eba224544655b8fd9b610fa44ff24915e
James Craine <jamesdcraine@gmail.com>
2020-04-28 11:20:07 -0400

    additions

---
+++ Develop/routes/htmlRoutes.js
@@ -0,0 +1,12 @@
+var path = require("path");
+
+module.exports = function(app) {
+  // called when Countinue Workout or new Workout is clicked in index.html
+  app.get("/exercise", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+  });
+
+  app.get("/stats", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+  });
+};
